d93e30dcbbd1e8296e939ae1cecf17700f61597f
Uuganaaa12 <uugnaauugaa56@gmail.com>
2025-12-07 14:51:59 +0800

    Add validation to calculateTotal function

--- lib/total.dart
+++ lib/total.dart
@@ -1,5 +1,7 @@
+
 part of area;
 
 double calculateTotal(double doseMg, double frequency, double days) {
+  if (doseMg <= 0 || frequency <= 0 || days <= 0) return 0.0;
   return doseMg * frequency * days;
 }
