71a9c691073b22e617f64939c1b51cf0374badb7
Uuganaaa12 <uugnaauugaa56@gmail.com>
2025-12-07 14:55:27 +0800

    Refactor calculateTotal to use num type

--- lib/total.dart
+++ lib/total.dart
@@ -1,7 +1,5 @@
-
 part of area;
 
-double calculateTotal(double doseMg, double frequency, double days) {
-  if (doseMg <= 0 || frequency <= 0 || days <= 0) return 0.0;
-  return doseMg * frequency * days;
+num calculateTotal(num doseMg, num doseMgPerDay, num days) {
+  return doseMg * doseMgPerDay * days;
 }
