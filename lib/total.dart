b92efbcbe0af650a9e9bb34dd177f258e698b983
Uuganaaa12 <uugnaauugaa56@gmail.com>
2025-12-07 14:59:51 +0800

    Add calculateTotalInt function for integer doses

--- lib/total.dart
+++ lib/total.dart
@@ -1,5 +1,6 @@
 part of area;
 
-num calculateTotal(num doseMg, num doseMgPerDay, num days) {
-  return doseMg * doseMgPerDay * days;
+int calculateTotalInt(int doseMg, int frequency, int days) {
+  if (doseMg <= 0 || frequency <= 0 || days <= 0) return 0;
+  return doseMg * frequency * days;
 }
